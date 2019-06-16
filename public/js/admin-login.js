714cf1be13da45f118a6c8cc46f1fa85d0a67c62
Adetola Bewaji <adetola013@gmail.com>
2019-06-17 00:07:55 +0100

    login logic added for admin user

---
+++ public/js/admin-login.js
@@ -0,0 +1,48 @@
+window.onload = utilityLoginInAdmin;
+
+function utilityLoginInAdmin(){
+  $("#btnSubmit").click(apiData);
+}
+
+
+function Userdetails(){
+  this.username = $('input#admin_username').val(),
+  this.password = $('input#admin_password').val()
+}
+
+const loginToAdminHome = function(dict){
+  var [userType, user] = [dict["1"], dict["2"]]
+  username = new Userdetails().username
+  password = new Userdetails().password
+  user.forEach((elm, index)=>{
+	 if (elm["username"] === username && elm["password"] === password){
+	 	type = userType[elm["user-type"]-1].name
+        // console.log(elm["username"], elm["password"], type)
+	    localStorage.setItem('Userid', elm.id);
+	 	return goToAdminHome(type);
+	 }
+  })
+}
+
+const goToAdminHome = function(uiLocation){
+    window.location.href = `http://localhost:3000/${uiLocation}/index.html`;
+    window.location.reload = true;
+}
+
+dict = {};
+var i=1;
+
+const obj = (param, r)=>{
+   dict[String(i++)] = param;
+	if (i === r){
+	  loginToAdminHome(dict);
+	}
+}
+
+const apiData = ()=>{
+    //Populate .select-make
+    adminLogin(obj, `http://localhost:3000/user-type`);    
+	// Populate .select-year-from
+    adminLogin(obj, `http://localhost:3000/users`);
+}
+
