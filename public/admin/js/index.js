9b998b55039238e1f01fcf01b91ac4dcca8b765f
Adetola Bewaji <adetola013@gmail.com>
2019-07-12 16:57:58 +0100

    added new method to update user last-login in db

--- public/admin/js/index.js
+++ public/admin/js/index.js
@@ -4,46 +4,26 @@ function utilityAdmins(){
   apiAdminData();
 }
 
-function adminDetails(dataSet, apiEndpoint){
-
-   var data_file =  apiEndpoint;
-   var http_request = new XMLHttpRequest();
-   
-   httpCrossBrowser(http_request);
-
-   http_request.onreadystatechange = function(){
-      if (http_request.readyState == 4) {
-        // Javascript function JSON.parse to parse data
-        var jsonObj = JSON.parse(http_request.responseText);
-
-            dataSet(jsonObj, 4);
-      }
-    } 
-
-    http_request.open("GET", data_file, true);
-    http_request.send();
-}
-
 function adminusers(dict) {
-  var [permission, user, profile] = [dict["1"], dict["2"], dict["3"]];
+  var [permission, user] = [dict["1"], dict["2"]];
   var tableContent = "";
   $.each(user, function(index, elm) {
 
     if (elm["user-type"] !== 3){
 
-        type = permission[elm["user-type"]-1]["name"];
-        try {
-            firstname = profile[elm["profile"]-1].firstname;
-            lastname = profile[elm["profile"]-1].lastname;
-            fname = `${firstname} ${lastname}`;
+        type = permission[elm["user_type"]-1]["name"];
+        fname = elm.fullname;
+        // try {
+            // firstname = profile[elm["profile"]-1].firstname;
+            // lastname = profile[elm["profile"]-1].lastname;
+            //fname = `${firstname} ${lastname}`;
 
-            index = elm["profile"]-1
-            profile.splice(index, 1)   // remove spotted index value 
+            // index = elm["profile"]-1
+            // profile.splice(index, 1)   // remove spotted index value 
 
-        } catch (e){
-            var fname = elm.username;    
-
-        }
+        // } catch (e){
+        //     var fname = elm.username;    
+        // }
              
         tableContent += '<tr>';
         tableContent += '<td><a href="#" class="linkshowuser" rel="' + elm.id + '">' + fname + '</a></td>';
@@ -75,7 +55,5 @@ const apiAdminData = ()=>{
     adminDetails(obj, `http://localhost:3000/user-type`);    
     // Populate users
     adminDetails(obj, `http://localhost:3000/users`);
-    // Populate profile
-    adminDetails(obj, `http://localhost:3000/profile`);
 }
 
